1ad614e0cf57a1d02d0637c838870f2b37888647
mohakmisra8 <63446965+mohakmisra8@users.noreply.github.com>
2026-07-12 13:04:13 +0100

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        // iterate through array if count of num in array > 1
+        Set<Integer> set = new HashSet<>();
+        for(int i:nums) {
+            if(set.contains(i)) {
+                return true;
+            }
+            set.add(i);
+        }
+        return false;
+        
+    }
+}
