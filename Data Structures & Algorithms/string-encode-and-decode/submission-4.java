0c7726ef6aab6c04246784f81cc2a46b6e3b5b39
mohakmisra8 <63446965+mohakmisra8@users.noreply.github.com>
2026-08-01 08:12:04 +0100

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    private static final Character HASH = '#';
+
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for(String s: strs) {
+            sb.append(s.length()).append(HASH).append(s);
+        }
+
+        return sb.toString();
+
+    }
+
+    public List<String> decode(String str) {
+        List<String> res = new ArrayList<>();
+        int i = 0;
+        while(i < str.length()) {
+            int j = i;
+            while(str.charAt(j) !=  HASH) {
+                j++;
+            }
+
+            int len = Integer.parseInt(str.substring(i,j));
+            res.add(str.substring(j+1, j+1+len));
+            i = j+1+len;
+
+        }
+        return res;
+
+    }
+}
