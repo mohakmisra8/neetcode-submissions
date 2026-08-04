56aa68d5d45c510bef71dc09fb0a0c8ab5fa400d
mohakmisra8 <63446965+mohakmisra8@users.noreply.github.com>
2026-08-05 00:39:19 +0100

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        // floyd find where the cycle is detected
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+
+        // find the cycle entrance (= duplicate)
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+        
+    }
+}
