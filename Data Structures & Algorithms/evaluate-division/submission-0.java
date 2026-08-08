3c9ffe6070e911d5e3e675346bf6b68d7d2a0f32
mohakmisra8 <63446965+mohakmisra8@users.noreply.github.com>
2026-08-08 22:20:30 +0100

    Add: evaluate-division - submission-0

---
+++ Data Structures & Algorithms/evaluate-division/submission-0.java
@@ -0,0 +1,53 @@
+class Solution {
+    public double[] calcEquation(
+        List<List<String>> equations, double[] values, List<List<String>> queries) {
+        Map<String, Map<String, Double>> graph = new HashMap<>();
+
+        // iterate over equations and add to map
+        for (int i = 0; i < equations.size(); i++) {
+            String a = equations.get(i).get(0);
+            String b = equations.get(i).get(1);
+            double v = values[i];
+            // get a's neighbor map (make an empty one if it doesn't exist yet),
+            // then put b into it and weight v
+            graph.computeIfAbsent(a, k -> new HashMap<>()).put(b, v);
+            // same thing but backwards
+            graph.computeIfAbsent(b, k -> new HashMap<>()).put(a, 1.0/v);
+        }
+
+        double[] res = new double[queries.size()];
+        for (int i = 0; i< queries.size(); i++) {
+            String c = queries.get(i).get(0);
+            String d = queries.get(i).get(1);
+            res[i] = dfs(c, d, new HashSet<>(), graph);
+        }
+        return res;
+
+    }
+
+    private double dfs(String curr, String target, Set<String> visited, Map<String, Map<String, Double>> graph) {
+        if (!graph.containsKey(curr)) {
+            return -1.0;
+        }
+
+        if(curr.equals(target)) {
+            return 1.0;
+        }
+        visited.add(curr);
+
+        for(Map.Entry<String, Double> nb: graph.get(curr).entrySet()) {
+            if(visited.contains(nb.getKey())) {
+                continue;
+            }
+
+            double sub = dfs(nb.getKey(), target, visited, graph);
+
+            if(sub != -1) {
+                return sub * nb.getValue();
+            }
+        }
+
+        return -1;
+
+    }
+}
