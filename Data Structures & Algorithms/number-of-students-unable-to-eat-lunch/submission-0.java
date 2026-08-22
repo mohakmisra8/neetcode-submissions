97aea58deb7059f6cdbd668290dba9f2b3569af3
mohakmisra8 <63446965+mohakmisra8@users.noreply.github.com>
2026-08-22 11:40:23 +0100

    Add: number-of-students-unable-to-eat-lunch - submission-0

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int n = students.length;
+        Queue<Integer> q = new LinkedList<>();
+        for(int s:students) {
+            q.offer(s);
+        }
+
+        int res = n;
+        for(int sandwich:sandwiches) {
+            int count = 0;
+            while(count < n && q.peek() != sandwich) {
+                q.offer(q.poll());
+                count++;
+            }
+            if(q.peek() == sandwich) {
+                q.poll();
+                res--;
+                
+            } else{
+                break;
+            }
+        }
+
+        return res;
+        
+    }
+}
