6e9be420924cefd0e082c6e105f010927fdb1b9e
mohakmisra8 <63446965+mohakmisra8@users.noreply.github.com>
2026-08-23 22:40:27 +0100

    Add: binary-tree-zigzag-level-order-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-zigzag-level-order-traversal/submission-0.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        boolean leftToRight = true; // direction flag, flips each lvel
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                level.add(node.val); // record value
+
+                if (node.left != null) {
+                    q.offer(node.left); // enqueue children
+                }
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
+            }
+
+            if(!leftToRight) {
+                Collections.reverse(level);
+            }
+            res.add(level);
+            leftToRight = !leftToRight;
+        }
+        return res;
+    }
+}
