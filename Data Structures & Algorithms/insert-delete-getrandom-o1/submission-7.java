0d4b77077f0923ce31d4cab13db558e47723a23d
mohakmisra8 <63446965+mohakmisra8@users.noreply.github.com>
2026-08-23 10:46:55 +0100

    Add: insert-delete-getrandom-o1 - submission-7

---
+++ Data Structures & Algorithms/insert-delete-getrandom-o1/submission-7.java
@@ -0,0 +1,47 @@
+class RandomizedSet {
+    private HashMap<Integer, Integer> map;
+    private List<Integer> list;
+    private Random random;
+
+    public RandomizedSet() {
+        map = new HashMap<>();
+        list = new ArrayList<>();
+        random = new Random();
+    }
+
+    public boolean insert(int val) {
+        if (map.containsKey(val)) {
+            return false;
+        }
+
+        map.put(val, list.size());
+        list.add(val);
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) {
+            return false;
+        }
+        int index = map.get(val);
+        int lastVal = list.get(list.size()-1);
+        map.put(lastVal, index);
+        list.set(index, lastVal);
+        list.remove(list.size()-1);
+        map.remove(val);
+
+        return true;
+    }
+
+    public int getRandom() {
+        return list.get(random.nextInt(list.size()));
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
