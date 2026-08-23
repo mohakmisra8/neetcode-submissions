0702224224b8fcfb4210c7a50b0d66625617ac66
mohakmisra8 <63446965+mohakmisra8@users.noreply.github.com>
2026-08-23 10:59:57 +0100

    Add: meeting-scheduler - submission-1

---
+++ Data Structures & Algorithms/meeting-scheduler/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    // two pointer merge
+    public List<Integer> minAvailableDuration(int[][] slots1, int[][] slots2, int duration) {
+        Arrays.sort(slots1, (a,b) -> a[0]-b[0]);
+        Arrays.sort(slots2, (a,b) -> a[0]-b[0]);
+
+        int i = 0;
+        int j = 0;
+        while (i < slots1.length && j < slots2.length) {
+            // obverlap of thw two current slots
+            int start = Math.max(slots1[i][0], slots2[j][0]);
+            int end = Math.min(slots1[i][1], slots2[j][1]);
+
+            if(end - start >= duration) {
+                return Arrays.asList(start, start+duration);
+            }
+
+            // advance the pointer whose slot ends earlier - it can't help future overlaps
+            if(slots1[i][1] < slots2[j][1]) {
+                i++;
+            } else {
+                j++;
+            }
+
+        }
+
+        return new ArrayList<>();
+
+
+    }
+}
