6dd1bc151a2193f3b77f4d4e715b080f163c6ca2
mohakmisra8 <63446965+mohakmisra8@users.noreply.github.com>
2026-08-23 09:36:13 +0100

    Add: buy-and-sell-crypto - submission-9

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-9.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for(int i = 0; i< prices.length;i++) {
+            int tempProfit = prices[i] - minPrice;
+            maxProfit = Math.max(tempProfit, maxProfit);
+
+            if(prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+
+        }
+
+        return maxProfit;
+        
+    }
+}
