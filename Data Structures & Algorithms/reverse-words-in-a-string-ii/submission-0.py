6a096001a3be4b6bd2bbb2d90a1aa8178414c785
mohakmisra8 <63446965+mohakmisra8@users.noreply.github.com>
2026-09-16 23:55:03 +0100

    Add: reverse-words-in-a-string-ii - submission-0

---
+++ Data Structures & Algorithms/reverse-words-in-a-string-ii/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def reverseWords(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        # reverse full string
+        self.reverse(s, 0, len(s)-1)
+
+        # reverse each word
+        self.reverse_each_word(s)
+
+    def reverse(self, l: List[str], left: int, right: int) -> None:
+        while left < right:
+            l[left], l[right] = l[right], l[left]
+            left, right = left+1, right-1
+    
+    def reverse_each_word(self, l: List[str]) -> None:
+        n = len(l)
+        start = end = 0
+        
+        while start < n:
+            # go to the end of the word
+            while end < n and l[end] != ' ':
+                end +=1
+            # reverse word
+            self.reverse(l, start, end-1)
+            # move onto next word
+            start = end+1
+            end +=1
+        
