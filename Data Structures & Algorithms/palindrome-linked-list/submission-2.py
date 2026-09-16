db65b9d10a1341975a5d304061ec9608a914f6f3
mohakmisra8 <63446965+mohakmisra8@users.noreply.github.com>
2026-09-16 23:25:16 +0100

    Add: palindrome-linked-list - submission-2

---
+++ Data Structures & Algorithms/palindrome-linked-list/submission-2.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        self.cur = head
+
+        def rec(node):
+            if node is not None:
+                if not rec(node.next):
+                    return False
+                
+                if self.cur.val != node.val:
+                    return False
+                
+                self.cur = self.cur.next
+            return True
+        
+        return rec(head)
+        
